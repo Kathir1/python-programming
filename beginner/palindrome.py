e4b86ecffdd9770dd1d9f0572faa49e9f9b1ae3a
Kathir1 <36753350+Kathir1@users.noreply.github.com>
2018-07-23 12:04:34 +0530

    Create palindrome.py

---
+++ beginner/palindrome.py
@@ -0,0 +1,14 @@
+def main():
+ n=int(input("enter the number"))
+ temp=n
+ rev=0
+ while(n>0):
+    dig=n%10
+    rev=rev*10+dig
+    n=n//10
+ if(temp==rev):
+    print("Yes")
+ else:
+    print("No")
+if __name__ == '__main__':
+    main()
